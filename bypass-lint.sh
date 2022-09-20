6c9c22c691d4545935c3d89a114af38b0d00de61
Evan Chen <evan@evanchen.cc>
2022-09-20 13:18:09 -0400

    fix: update bypass-lint.sh properly

--- bypass-lint.sh
+++ bypass-lint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-git rev-parse HEAD > /tmp/otisweb.good
+GOOD_FILE="/tmp/${PWD##*/}.good"
+git rev-parse HEAD > $GOOD_FILE
 
 echo -e "-----------------------------------------------------------------------"
 echo -e "\033[1;31m$(git rev-parse HEAD)\033[0m had lint.sh bypassed"
 echo -e "-----------------------------------------------------------------------"
-
