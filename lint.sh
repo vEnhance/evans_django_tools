146aebd96501725347c73582b9a820040182eaf1
Evan Chen <evan@evanchen.cc>
2022-11-15 21:55:22 -0500

    feat: Add in prettier linting too yayyyy

--- lint.sh
+++ lint.sh
@@ -10,24 +10,26 @@ GOOD_FILE="/tmp/${PWD##*/}.good"
 
 COMMIT_ID=$(git rev-parse HEAD)
 readarray -t PY_FILES_ARRAY < <(git ls-files '*.py' | grep -v migrations/ | grep -v /apps.py)
+readarray -t HTML_FILES_ARRAY < <(git ls-files '*.html')
+readarray -t PRETTIER_FILES_ARRAY < <(git ls-files '*.css' '*.js' '*.md')
 readarray -t SPELL_FILES_ARRAY < <(git ls-files)
 
 if [ -f "$GOOD_FILE" ]; then
-	if [ "$COMMIT_ID" == "$(cat "$GOOD_FILE")" ]; then
-		echo -e "-----------------------------------------------------------------------"
-		echo -e "\033[1;32m$COMMIT_ID\033[0m was marked all-OK, exiting..."
-		echo -e "-----------------------------------------------------------------------"
-		exit 0
-	fi
+  if [ "$COMMIT_ID" == "$(cat "$GOOD_FILE")" ]; then
+    echo -e "-----------------------------------------------------------------------"
+    echo -e "\033[1;32m$COMMIT_ID\033[0m was marked all-OK, exiting..."
+    echo -e "-----------------------------------------------------------------------"
+    exit 0
+  fi
 fi
 
 if [ -f "$BAD_FILE" ]; then
-	if [ "$COMMIT_ID" == "$(cat "$BAD_FILE")" ]; then
-		echo -e "-----------------------------------------------------------------------"
-		echo -e "\033[1;33m$COMMIT_ID\033[0m was marked faulty, aborting..."
-		echo -e "-----------------------------------------------------------------------"
-		exit 1
-	fi
+  if [ "$COMMIT_ID" == "$(cat "$BAD_FILE")" ]; then
+    echo -e "-----------------------------------------------------------------------"
+    echo -e "\033[1;33m$COMMIT_ID\033[0m was marked faulty, aborting..."
+    echo -e "-----------------------------------------------------------------------"
+    exit 1
+  fi
 fi
 
 echo -e "-----------------------------------------------------------------------"
@@ -37,63 +39,63 @@ echo "${PY_FILES_ARRAY[@]}"
 echo -e "-----------------------------------------------------------------------"
 
 if [ "$1" == "--force" ]; then
-	echo -e "\033[1;31m]$COMMIT_ID\033[0m not being compared to upstream"
-	echo -e "---------------------------"
-	echo -e ""
+  echo -e "\033[1;31m]$COMMIT_ID\033[0m not being compared to upstream"
+  echo -e "---------------------------"
+  echo -e ""
 else
-	echo -e "\033[1;35mChecking against upstream ...\033[0m"
-	echo -e "---------------------------"
-	git fetch
-	if [ "$(git rev-list --count HEAD..@\{u\})" -gt 0 ]; then
-		echo -e "$FAILED_HEADER Upstream is ahead of you"
-		echo "$COMMIT_ID" >"$BAD_FILE"
-		exit 1
-	fi
-	echo -e ""
+  echo -e "\033[1;35mChecking against upstream ...\033[0m"
+  echo -e "---------------------------"
+  git fetch
+  if [ "$(git rev-list --count HEAD..@\{u\})" -gt 0 ]; then
+    echo -e "$FAILED_HEADER Upstream is ahead of you"
+    echo "$COMMIT_ID" >"$BAD_FILE"
+    exit 1
+  fi
+  echo -e ""
 fi
 
 echo -e "\033[1;35mRunning poetry install --sync ...\033[0m"
 echo -e "---------------------------"
 if ! poetry install --sync; then
-	echo -e "$FAILED_HEADER Dependency update failed"
-	echo "$COMMIT_ID" >"$BAD_FILE"
-	exit 1
+  echo -e "$FAILED_HEADER Dependency update failed"
+  echo "$COMMIT_ID" >"$BAD_FILE"
+  exit 1
 fi
 echo -e ""
 
 echo -e "\033[1;35mRunning spell-check ...\033[0m"
 echo -e "---------------------------"
 if ! codespell "${SPELL_FILES_ARRAY[@]}"; then
-	echo -e "$FAILED_HEADER Spell-check failed"
-	echo "$COMMIT_ID" >"$BAD_FILE"
-	exit 1
+  echo -e "$FAILED_HEADER Spell-check failed"
+  echo "$COMMIT_ID" >"$BAD_FILE"
+  exit 1
 fi
 echo -e ""
 
 echo -e "\033[1;35mRunning manage.py check ...\033[0m"
 echo -e "---------------------------"
 if ! python manage.py check; then
-	echo -e "$FAILED_HEADER python manage.py check failed"
-	echo "$COMMIT_ID" >"$BAD_FILE"
-	exit 1
+  echo -e "$FAILED_HEADER python manage.py check failed"
+  echo "$COMMIT_ID" >"$BAD_FILE"
+  exit 1
 fi
 echo -e ""
 
 echo -e "\033[1;35mRunning pyflakes ...\033[0m"
 echo -e "---------------------------"
 if ! pyflakes .; then
-	echo -e "$FAILED_HEADER pyflakes gave nonzero status"
-	echo "$COMMIT_ID" >"$BAD_FILE"
-	exit 1
+  echo -e "$FAILED_HEADER pyflakes gave nonzero status"
+  echo "$COMMIT_ID" >"$BAD_FILE"
+  exit 1
 fi
 echo -e ""
 
 echo -e "\033[1;35mMaking migrations ...\033[0m"
 echo -e "---------------------------"
 if ! python manage.py makemigrations | grep "No changes detected"; then
-	echo -e "$FAILED_HEADER I think you forgot a migration!"
-	echo "$COMMIT_ID" >"$BAD_FILE"
-	exit 1
+  echo -e "$FAILED_HEADER I think you forgot a migration!"
+  echo "$COMMIT_ID" >"$BAD_FILE"
+  exit 1
 fi
 python manage.py migrate
 echo -e ""
@@ -101,38 +103,56 @@ echo -e ""
 echo -e "\033[1;35mLinting files with yapf ...\033[0m"
 echo -e "---------------------------"
 if ! yapf -d "${PY_FILES_ARRAY[@]}"; then
-	echo -e "$FAILED_HEADER Some files that needed in-place edits, editing now..."
-	yapf --in-place "${PY_FILES_ARRAY[@]}"
-	echo -e "Better check your work!"
-	echo "$COMMIT_ID" >"$BAD_FILE"
-	exit 1
+  echo -e "$FAILED_HEADER Some files that needed in-place edits, editing now..."
+  yapf --in-place "${PY_FILES_ARRAY[@]}"
+  echo -e "Better check your work!"
+  echo "$COMMIT_ID" >"$BAD_FILE"
+  exit 1
+fi
+echo -e ""
+
+echo -e "\033[1;35mRunning djlint ...\033[0m"
+echo -e "---------------------------"
+if ! djlint --check "${HTML_FILES_ARRAY[@]}"; then
+  echo -e "$FAILED_HEADER djlint failed"
+  echo "$COMMIT_ID" >"$BAD_FILE"
+  exit 1
+fi
+echo -e ""
+
+echo -e "\033[1;35mRunning prettier ...\033[0m"
+echo -e "---------------------------"
+if ! prettier --check "${PRETTIER_FILES_ARRAY[@]}"; then
+  echo -e "$FAILED_HEADER prettier failed"
+  echo "$COMMIT_ID" >"$BAD_FILE"
+  exit 1
 fi
 echo -e ""
 
 echo -e "\033[1;35mRunning mypy ...\033[0m"
 echo -e "---------------------------"
 if ! mypy --ignore-missing-imports "${PY_FILES_ARRAY[@]}"; then
-	echo -e "$FAILED_HEADER mypy failed"
-	echo "$COMMIT_ID" >"$BAD_FILE"
-	exit 1
+  echo -e "$FAILED_HEADER mypy failed"
+  echo "$COMMIT_ID" >"$BAD_FILE"
+  exit 1
 fi
 echo -e ""
 
 echo -e "\033[1;35mRunning pyright ...\033[0m"
 echo -e "---------------------------"
 if ! pyright; then
-	echo -e "$FAILED_HEADER pyright failed"
-	echo "$COMMIT_ID" >"$BAD_FILE"
-	exit 1
+  echo -e "$FAILED_HEADER pyright failed"
+  echo "$COMMIT_ID" >"$BAD_FILE"
+  exit 1
 fi
 echo -e ""
 
 echo -e "\033[1;35mRunning coverage/tests ...\033[0m"
 echo -e "---------------------------"
 if ! coverage run manage.py test --shuffle 1337; then
-	echo -e "$FAILED_HEADER Unit tests did not check out"
-	echo "$COMMIT_ID" >"$BAD_FILE"
-	exit 1
+  echo -e "$FAILED_HEADER Unit tests did not check out"
+  echo "$COMMIT_ID" >"$BAD_FILE"
+  exit 1
 fi
 echo -e ""
 
