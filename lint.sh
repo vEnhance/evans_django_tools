f2e1a9727153b01ad64ea1d89f4e023411f9711f
Evan Chen <evan@evanchen.cc>
2023-02-03 09:03:13 -0800

    polish: lint.sh less verbose

--- lint.sh
+++ lint.sh
@@ -34,8 +34,7 @@ fi
 
 echo -e "-----------------------------------------------------------------------"
 echo -e "\033[1;36mTesting $COMMIT_ID\033[0m"
-echo -e "\033[1;34mWill typecheck the following ${#PY_FILES_ARRAY[@]} files...\033[0m"
-echo "${PY_FILES_ARRAY[@]}"
+echo -e "\033[1;34mWill echeck ${#PY_FILES_ARRAY[@]} Python files...\033[0m"
 echo -e "-----------------------------------------------------------------------"
 
 if [ "$1" == "--force" ]; then
